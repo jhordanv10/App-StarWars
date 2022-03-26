24251e18b1930603dfa6e0d713c2bce65cf70e8f
Jhordan Villamil <jhordanvilla1000@gmail.com>
2022-03-26 18:28:52 -0500

    Mejoras en codigo no usado y limpieza de código existente

--- src/components/atoms/Characters/index.jsx
+++ src/components/atoms/Characters/index.jsx
@@ -1,16 +1,12 @@
-import { useEffect, useState } from 'react'
 import './Characters.scss';
-import Modal from '../../molecules/Modal/Modal';
 
 export default function Characters({ url, handleClick }) {
 
-    
     const idCharacter = url.split('/').splice(-2, 1)
-    
-    
+
     return (
-            <li className='character-list'>
-                <button value={idCharacter} type='button' onClick={handleClick} className="character-list-button"> Character  {idCharacter}</button><br></br>
-            </li>
+        <li className='character-list'>
+            <button value={idCharacter} type='button' onClick={handleClick} className="character-list-button"> Character  {idCharacter}</button><br></br>
+        </li>
     )
 }
